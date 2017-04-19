f628ce21c44ff2b9146d5f8b5b9280d7e85eb452
Ian Bell <ian.h.bell@gmail.com>
2017-04-19 00:20:03 -0600

    Make the example for OpenMP work

--- main.cpp
+++ main.cpp
@@ -1,18 +1,57 @@
-#include <stdio.h>
-
-
 #include "CoolProp.h"
+#include "AbstractState.h"
 
+#include <stdio.h>
+#include <memory>
+#include <chrono>
 
+#include "omp.h"
+
+using namespace CoolProp;
+void do_one(int N){
+    
+    // This many threads are in the pool
+    int Nthread = 8;
+
+    std::vector<double> rho(N,0);
+    std::cout << "N:" << N << std::endl;
+    
+    // One instance of an AbstractState instance for each thread
+    std::vector<std::shared_ptr<AbstractState> > ASvec(N,
+                                                       std::shared_ptr<AbstractState>(AbstractState::factory("HEOS","CO2"))
+                                                       );
+    {
+        auto startTime = std::chrono::system_clock::now();
+        #pragma omp parallel for ordered
+        for(int i = 0; i < N; i++){
+            #pragma omp ordered
+            {
+                int j = i%omp_get_num_threads();
+                // std::cout << j << std::endl;
+                std::shared_ptr<AbstractState> &AS = ASvec[j];
+                AS->update(PT_INPUTS, 101325+i, 300);
+                rho[i] = AS->rhomolar();
+            }
+        }
+        auto endTime = std::chrono::system_clock::now();
+        std::cout << "With OpenMP: " << std::chrono::duration<double>(endTime - startTime).count() << std::endl;
+    }
+    {
+        auto startTime = std::chrono::system_clock::now();
+        for(int i = 0; i < N; i++){
+            std::shared_ptr<AbstractState> &AS = ASvec[i%4];
+            AS->update(PT_INPUTS, 101325+i, 300);
+            rho[i] = AS->rhomolar();
+        }
+        auto endTime = std::chrono::system_clock::now();
+        std::cout << "Without OpenMP: " << std::chrono::duration<double>(endTime - startTime).count() << std::endl;
+    }
+    std::cout << "\n\n";
+}
 int main(int argc, char *argv[])
 {
-    int i;
-    double H;
-#pragma omp parallel for private(H)
-    for(i = 0; i < 10; i++)
-    {
-        H = 0.0;
-        H = CoolProp::PropsSI("H", "P", 1000000.0, "T", 500.0, "CO2");
-        printf("i = %d H = %lf\n", i, H);
-    }
+    do_one(4);
+    do_one(100);
+    do_one(1000);
+    do_one(10000);
 }
