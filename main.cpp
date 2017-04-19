8e75d19aaa111438997391c1e525d93b5191db9d
Ian Bell <ian.h.bell@gmail.com>
2017-04-19 00:44:20 -0600

    Actually shows a speedup now, but the rather massive amount of allocation is a problem, and needs to be rethought

--- main.cpp
+++ main.cpp
@@ -17,25 +17,25 @@ void do_one(int N){
     std::cout << "N:" << N << std::endl;
     
     // One instance of an AbstractState instance for each thread
-    std::vector<std::shared_ptr<AbstractState> > ASvec(N,
-                                                       std::shared_ptr<AbstractState>(AbstractState::factory("HEOS","CO2"))
-                                                       );
-    {
+    std::vector<std::shared_ptr<AbstractState> > ASvec;
+    for(int i = 0; i < N; ++i){ ASvec.push_back(std::shared_ptr<AbstractState>(AbstractState::factory("HEOS","CO2"))); }
+    
+    {   
         auto startTime = std::chrono::system_clock::now();
-        #pragma omp parallel for ordered
+        #pragma omp parallel for
         for(int i = 0; i < N; i++){
-            #pragma omp ordered
-            {
-                int j = i%omp_get_num_threads();
-                // std::cout << j << std::endl;
-                std::shared_ptr<AbstractState> &AS = ASvec[j];
-                AS->update(PT_INPUTS, 101325+i, 300);
-                rho[i] = AS->rhomolar();
-            }
+
+            // int j = i%omp_get_num_threads(); // You could also try to use a smaller set of AbstractState instances, but that requires synchronization to ensure two threads don't write to the same abstractstate, or other intelligent means
+            std::shared_ptr<AbstractState> &AS = ASvec[i];
+            AS->update(PT_INPUTS, 101325+i, 300);
+            rho[i] = AS->rhomolar();
+            // #pragma omp critical
+            // std::cout << j << " " << rho[i] << std::endl;
         }
         auto endTime = std::chrono::system_clock::now();
         std::cout << "With OpenMP: " << std::chrono::duration<double>(endTime - startTime).count() << std::endl;
     }
+    
     {
         auto startTime = std::chrono::system_clock::now();
         for(int i = 0; i < N; i++){
@@ -46,7 +46,7 @@ void do_one(int N){
         auto endTime = std::chrono::system_clock::now();
         std::cout << "Without OpenMP: " << std::chrono::duration<double>(endTime - startTime).count() << std::endl;
     }
-    std::cout << "\n\n";
+    std::cout << "\n";
 }
 int main(int argc, char *argv[])
 {
@@ -54,4 +54,5 @@ int main(int argc, char *argv[])
     do_one(100);
     do_one(1000);
     do_one(10000);
+    do_one(100000);
 }
